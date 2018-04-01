34e08ca7d11f5188827324084c1dbc7004398ed0
bhajendra <bhajendra.pund1@gmail.com>
2018-04-01 21:22:50 +0530

    Customerlist api integration and Form integration;

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {IndexPage} from "../index/index";
 import {RegisterPage} from "../register/register";
+import { ServicesProvider } from '../../providers/services/services';
 
 /**
  * Generated class for the ContactPage page.
@@ -15,23 +16,64 @@ import {RegisterPage} from "../register/register";
   templateUrl: 'contact.html',
 })
 export class ContactPage {
+  userId: string;
+  customerList: object[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public services: ServicesProvider
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactPage');
   }
 
+  ionViewWillEnter(){
+    this.services.getUserData().then(data=>{
+      this.userId = data.user_id;
+      this.getCustomerList();
+    });
+    
+  }
+
+  getCustomerList(){
+    this.services.get(`customers/getcustomerlist.php?user_id=${this.userId}`).subscribe((res:any)=>{
+      console.log("=== res ===>", res);
+      if(res.success){
+        this.customerList = res.data.customer_list || [];
+      }else{
+        this.services.createWaring('Error',res.error);
+      }
+    }, (err) => {
+      console.log("Error", err);
+      const errMsg = err.message || 'Something is wrong with the network.';
+      this.services.createWaring('Error', errMsg);
+    });
+  }
+
   dismiss(){
     this.navCtrl.setRoot(IndexPage);
+  }
 
+  onEdit(cust: any){
+    console.log("== Selected customer =-===>", cust);
+    this.navCtrl.push(RegisterPage, cust);
+  }
+
+  onDelete(cust: any){
+    let data = {
+      customer_id: cust.customer_id,
+      user_id: this.userId
+    };
+    this.services.post(`customers/removecustomer.php`,data).subscribe((res:any)=>{
+      console.log("=== delete res ===>", res);
+    })
   }
 
   addcustomer(){
-
-    this.navCtrl.setRoot(RegisterPage)
-
+    this.navCtrl.push(RegisterPage)
   }
 
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import {IndexPage} from "../index/index";
 import {ContactPage} from "../contact/contact";
-import {Http} from "@angular/http";
 import {InsertpaperPage} from "../insertpaper/insertpaper";
+import { ServicesProvider } from '../../providers/services/services';
 
 /**
  * Generated class for the RegisterPage page.
@@ -17,8 +18,48 @@ import {InsertpaperPage} from "../insertpaper/insertpaper";
   templateUrl: 'register.html',
 })
 export class RegisterPage {
+  title: string = "Add Customer";
+  custForm: FormGroup;
+  apiUrl: string = "insertcustomer.php";
+  submited: boolean = false;
+  userId: string;
+  custData: object = {
+    name: '',
+    email: '',
+    address: '',
+    mobile: '',
+    alt_contact: ''
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public http: Http) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public services: ServicesProvider,    
+    public formBuilder: FormBuilder    
+  ) {
+    this.custForm = formBuilder.group({
+      name: [null, Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required])],
+      address: ['', Validators.compose([Validators.required])],
+      mobile: ['', Validators.compose([Validators.required])],
+      alt_contact: ['']      
+    });
+  }
+
+  ionViewWillEnter(){
+    console.log("=== Params Data ===>", this.navParams);
+    let custData = this.navParams.data;
+    this.services.getUserData().then(data=>{
+      this.userId = data.user_id;
+      if(custData){
+        this.custData = custData;
+        this.title = "Profile";
+        this.apiUrl = "updatecustomer.php"
+      }else{
+        this.title = "Add Customer";
+        this.apiUrl = "insertcustomer.php"
+      }
+    });
   }
 
   ionViewDidLoad() {
@@ -26,10 +67,30 @@ export class RegisterPage {
   }
 
   dismiss(){
-    this.navCtrl.setRoot(ContactPage);
+    this.navCtrl.pop();
 
   }
   submit(){
+    if (!this.custForm.valid) {
+      this.submited = true;
+      return false;
+    }
+
+    
+
+    this.services.post(this.apiUrl, this.custForm.value).subscribe((res: any) => {
+      if (res.success) {
+        this.services.setUserData(res.data);
+        this.services.setLogged();
+        this.navCtrl.setRoot (IndexPage);        
+      }else{
+        this.services.createWaring('Error',res.error);
+      }
+    }, (err) => {
+      console.log("Error", err);
+      const errMsg = err.message || 'Something is wrong with the network.';
+      this.services.createWaring('Error', errMsg);
+    });
 
     this.navCtrl.setRoot(InsertpaperPage);
 
